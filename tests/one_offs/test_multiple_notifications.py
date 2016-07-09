4e574c446aa06e84d5996415bdeda1dbb7717d22
Andrew Tonner <rakslice@gmail.com>
2016-07-09 14:02:36 -0700

    manual test script for notifications; got the functional fix

---
+++ tests/one_offs/test_multiple_notifications.py
@@ -0,0 +1,23 @@
+import sys
+
+import windows_10_toast_notifications
+
+
+def main():
+    notifier = windows_10_toast_notifications.WindowsBalloonTip(debug_output=True)
+
+    for i in xrange(3):
+        text = "this is notification %s" % i
+        title = "test multiple notifications %s" % i
+        notifier.balloon_tip(title, text)
+
+    pause()
+
+
+def pause(msg="pause, press return"):
+    print msg
+    sys.stdin.readline()
+
+
+if __name__ == "__main__":
+    main()
--- windows_10_toast_notifications.py
+++ windows_10_toast_notifications.py
@@ -22,7 +22,9 @@ DEBUG = False
 
 
 class WindowsBalloonTip:
-    def __init__(self):
+    def __init__(self, window_title="Taskbar", debug_output=False):
+        self.debug_output = debug_output
+
         message_map = {win32con.WM_DESTROY: self.on_destroy,
                        OUR_NOTIFICATION_WM: self.on_notification_message,
                        }
@@ -34,6 +36,11 @@ class WindowsBalloonTip:
         wc.lpfnWndProc = message_map  # could also specify a wndproc.
         self.classAtom = RegisterClass(wc)
         self.last_callback = None
+        self.hwnd = None
+
+        self.window_title = window_title
+
+        self.init_window(window_title)
 
     # noinspection PyUnusedLocal,PyMethodMayBeStatic
     def on_destroy(self, hwnd, msg, wparam, lparam):
@@ -55,18 +62,50 @@ class WindowsBalloonTip:
         elif lparam == 1028:
             PostQuitMessage(0)
 
+    def init_window(self, window_title):
+        assert window_title is not None
+
+        # assert self.hwnd is None, "already initialized"
+
+        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+        hwnd = CreateWindow(self.classAtom, window_title, style, 0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, 0, 0, self.hinst, None)
+        UpdateWindow(hwnd)
+
+        self.hwnd = hwnd
+        # self.nid = nid
+        # self.hicon = hicon
+
+    def log(self, msg):
+        if self.debug_output:
+            print msg
+
     def balloon_tip(self, title, msg, callback=None):
         self.last_callback = callback
 
-        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-        hwnd = CreateWindow(self.classAtom, "Taskbar", style, 0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, 0, 0, self.hinst, None)
-        UpdateWindow(hwnd)
+        # self.init_window(self.window_title)
+
+        hwnd = self.hwnd
+        # nid = self.nid
+        # hicon = self.hicon
+        self.log("hwnd: %r" % hwnd)
 
         hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+        assert hicon
+        self.log(hicon)
 
-        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
-        nid = (hwnd, 0, flags, OUR_NOTIFICATION_WM, hicon, 'Tooltip')
+        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP | NIF_INFO
+        # flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
+        # nid = (hwnd, 0, flags, OUR_NOTIFICATION_WM, hicon, self.window_title)
+        nid = (hwnd, 0, flags, OUR_NOTIFICATION_WM, hicon, self.window_title, msg, 200, title, NIIF_INFO)
+
+        self.log(nid)
+
+        self.log("NIM_ADD")
         Shell_NotifyIcon(NIM_ADD, nid)
-        Shell_NotifyIcon(NIM_MODIFY, (hwnd, 0, NIF_INFO, OUR_NOTIFICATION_WM, hicon, 'Balloon Tooltip', msg, 200, title, NIIF_INFO))
+        # Shell_NotifyIcon(NIM_MODIFY, (hwnd, 0, NIF_INFO, OUR_NOTIFICATION_WM, hicon, 'Balloon Tooltip', msg, 200, title, NIIF_INFO))
 
+        self.log("PumpMessages()")
         PumpMessages()  # Handle window messages in main loop until something calls PostQuitMessage
+
+        self.log("NIM_DELETE")
+        Shell_NotifyIcon(NIM_DELETE, nid)
